5102cd964afa270728f670b9a3f34878bd96aa60
root <ymanshur@gmail.com>
2022-12-05 14:56:18 +0700

    initial commit

---
+++ project/exercise-1.rb
@@ -0,0 +1,9 @@
+puts "Hi, my name is Manshur"
+print "What is your name? "
+name = gets.chomp
+puts "Hi there #{name}, great to meet you!"
+
+question = gets
+if test
+    
+end
---
+++ project/hello.rb
@@ -0,0 +1,4 @@
+puts "Hello World"
+print "What is your name? "
+name = gets.chomp
+puts "Hi #{name}!" # interpolate variable
