9c956464a8a8ce93c69d90661c7137770fd37874
Yusuf Manshur <ymanshur@gmail.com>
2022-12-05 17:22:11 +0700

    refactor exercise_2.rb

--- project/exercise_2.rb
+++ project/exercise_2.rb
@@ -1,24 +1,24 @@
 # frozen_string_literal: true
 
-def add(a, b)
-    a + b
+def add(num1, num2)
+  num1 + num2
 end
 
-def multiply3(a, b, c)
-    a * b * c
+def multiply3(num1, num2, num3)
+  num1 * num2 * num3
 end
 
 def describe_temperature(value)
-    if value > 85
-        "It's hot"
-    else
-        "It's not hot"
-    end
-    # "The temperature outside is #{value}"
+  if value > 85
+    "It's hot"
+  else
+    "It's not hot"
+  end
+  # "The temperature outside is #{value}"
 end
 
 def divide(num, den)
-    num.to_f / den
+  num.to_f / den
 end
 
 # puts describe_temperature(85)
