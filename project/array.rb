05b459ae8ae2810f245821eb88c7e2b871ef350f
Yusuf Manshur <ymanshur@gmail.com>
2022-12-06 12:39:57 +0700

    add 'find', 'map' , 'sort' with block and 'each_with_index' for array block

--- project/array.rb
+++ project/array.rb
@@ -1,6 +1,6 @@
 # frozen_string_literal: true
 
-animals = %w(duck horse dog cat cat)
+animals = %w[duck horse dog cat cat]
 
 # for animal in animals do
 #   p animal
@@ -11,9 +11,24 @@ animals = %w(duck horse dog cat cat)
 # end
 animals.each { |animal| p animal }
 
-p animals.include?('cat')
-p animals.size
-p animals.sort
-p animals
-p animals.reverse
-p animals.uniq
+# p animals.include?('cat')
+# p animals.size
+# p animals.sort
+# p animals
+# p animals.reverse
+# p animals.uniq
+
+puts animals.find do |animal|
+  # animal[0] == 'd'
+  animal.length < 4
+end
+
+puts animals.map(&:upcase)
+
+puts animals.sort do |a, b|
+  a.length <=> b.length
+end
+
+animals.each_with_index do |animal, index|
+  puts "#{index}: #{animal}"
+end
