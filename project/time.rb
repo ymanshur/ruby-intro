2d6590b03042d87e26058a549bbb48269d04cf53
Yusuf Manshur <ymanshur@gmail.com>
2022-12-06 16:21:44 +0700

    add time.rb

---
+++ project/time.rb
@@ -0,0 +1,12 @@
+# frozen_string_literal: true
+
+def print_time_24
+  puts Time.now.strftime('%H:%M')
+end
+
+def print_time_12
+  puts Time.now.strftime('%I:%M %P')
+end
+
+print_time_24
+print_time_12
