c51956b6b294baa3a466a03361adfe0d3edd02bd
Yusuf Manshur <ymanshur@gmail.com>
2022-12-06 16:25:15 +0700

    add Clock class

--- project/clock.rb
+++ project/clock.rb
@@ -1,6 +1,17 @@
 # frozen_string_literal: true
 
-loop do
-  print "\r" + Time.now.strftime('%H:%M:%S on %B %d, %Y')
-  sleep 1
+class Clock
+  def run
+    loop do
+      print "\r#{time_string}"
+      sleep 1
+    end
+  end
+
+  def time_string
+    Time.now.strftime('%H:%M:%S on %B %d, %Y')
+  end
 end
+
+clock = Clock.new
+clock.run
